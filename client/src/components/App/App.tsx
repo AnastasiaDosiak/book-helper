b81a0a16dba197081ad6ac98eab4762f374a57d3
AnastasiaDosiak <nastia.dos@gmail.com>
2021-08-15 22:21:59 +0300

    added eslint, babelrc, configuration

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,15 +1,13 @@
-import React, {memo} from "react";
-import InputSelect from "../Form/Form";
-import Instructions from "../Instructions/Instructions";
-import "./App.scss";
+import React, { memo } from 'react';
+import InputSelect from '../Form/Form';
+import Instructions from '../Instructions/Instructions';
+import './App.scss';
 
-const App: React.FC = () => {
-    return (
-        <div className="App">
-            <Instructions/>
-            <InputSelect/>
-        </div>
-    );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <Instructions />
+    <InputSelect />
+  </div>
+);
 
 export default memo(App);
